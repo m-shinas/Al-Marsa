a9f74ac1cbb7fe176a7972a375f6cfaaa1886728
Mohammed Shinas <mshinas555@gmail.com>
2025-03-07 10:07:43 +0530

    hero video added locally

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ubuntu_sans } from "./fonts";
-import VideoComponent from "./VideoComponent";
 import { Suspense } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
@@ -9,7 +8,19 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 const Hero = () => (
   <div className="relative w-full h-screen bg-gradient-to-b from-[#0c0333]/30 to-[#0c0333]/30 py-0 px-[5%] flex items-center justify-center">
     <Suspense fallback={<FontAwesomeIcon icon={faSpinner} spinPulse color="#fff" size="4x"/>}>
-      <VideoComponent fileName="al-marsa-hero-NjgB5KXyQ4vcJkVSTJWMiYOPyn1Fjo.mp4" />
+      {/* <VideoComponent fileName="al-marsa-hero-NjgB5KXyQ4vcJkVSTJWMiYOPyn1Fjo.mp4" /> */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover -z-[1] "
+        preload="none"
+        aria-label="Video player"
+    >
+        <source src="/al-marsa-hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </Suspense>
     
     <div className="text-center">
